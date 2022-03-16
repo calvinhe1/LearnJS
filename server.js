09b14ff80eb526c900b23bfe3a57b9a1d542a352
Calvin <cal.he@mail.utoronto.ca>
2022-03-16 15:36:09 -0400

    Fixed issue in server

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(express.static(path.join(__dirname, '/pub')))
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-	log(`Listening on port ${port}...`)
+	console.log(`Listening on port ${port}...`)
 })
