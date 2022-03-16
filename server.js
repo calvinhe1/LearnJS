d30403a7ed65301112097941e5f62159ff447d02
Calvin <cal.he@mail.utoronto.ca>
2022-03-16 15:08:14 -0400

    Commiting before creating heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+// Setting up a static directory for the files in /pub
+// using Express middleware.
+app.use(express.static(path.join(__dirname, '/pub')))
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+})
